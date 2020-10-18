ed769b87a74807fb4f15ec3e2113cea648052ebf
FreezerNick <nl@f2p-entertainment.de>
2020-10-18 11:27:20 +0200

    Update Build.cs

--- Source/MoveableUMGWindow/MoveableUMGWindow.Build.cs
+++ Source/MoveableUMGWindow/MoveableUMGWindow.Build.cs
@@ -8,25 +8,11 @@ public class MoveableUMGWindow : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"UMG",
 			}
 			);
 			
@@ -38,15 +24,6 @@ public class MoveableUMGWindow : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
 			}
 			);
 	}
